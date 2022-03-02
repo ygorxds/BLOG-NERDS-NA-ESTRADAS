1d0dd665cafd372adf8ce784f226a9ceaea1fac0
ygorpsa <ygordesa2010@hotmail.com>
2022-03-02 14:28:27 -0300

    maintenance screen

---
+++ script.js
@@ -0,0 +1,8 @@
+function youtubeClick() {
+  window.open("https://www.youtube.com/c/Nerdsnaestrada");
+}
+
+function instaClick() {
+window.open("https://www.instagram.com/nerdsnaestradaoficial/")
+
+}
